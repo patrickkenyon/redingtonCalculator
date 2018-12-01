1942c83a46094bf30b040faa30e7192f6a7506e9
Patrick <patrick.kenyon@gmail.com>
2018-12-01 17:36:53 +0000

    added some js to send and validate form

---
+++ public/js/calculator.js
@@ -0,0 +1,95 @@
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault()
+
+    document.querySelector('.statusMessage').textContent = ''
+
+    document.querySelectorAll('.error').forEach(function (error) {
+        error.remove()
+    })
+
+    document.querySelectorAll('.formField').forEach(function (formField) {
+        let field = formField.querySelector('.field')
+
+        let error = document.createElement('span')
+        error.classList.add('error')
+
+        if (field.dataset.required) {
+            if (!validateRequired(field.value)) {
+                error.innerText += 'Field is required '
+                formField.appendChild(error)
+            }
+        }
+        if (field.dataset.postcode) {
+            if (!validatePostcode(field.value)) {
+                error.innerText += 'Invalid postcode'
+                formField.appendChild(error)
+            }
+        }
+    })
+
+    if (!document.querySelector('.error')) {
+        const formData = getFormData()
+        sendRequest(formData)
+    }
+})
+
+/**
+ * Checks if input is blank
+ *
+ * @param input value of field to be checked
+ * @returns {boolean} false if blank, else true
+ */
+function validateRequired(input) {
+    return (input !== '')
+}
+
+
+/**
+ * Retrieves values from form fields
+ *
+ * @returns {object} keys as field names & values as field values
+ */
+function getFormData() {
+    let formData = {}
+    const fieldWrappers = document.querySelectorAll(".formField")
+
+    fieldWrappers.forEach(function(fieldWrapper) {
+        let field = fieldWrapper.querySelector('.field')
+        formData[field.name] = field.value
+    })
+    return formData
+}
+
+/**
+ * Makes ajax request sending form data
+ *
+ * @param formData {object} values from form
+ * @returns {Promise<any>} ajax response
+ */
+async function fetchData(formData) {
+    let response = await fetch('/newCalculation', {
+        method: 'post',
+        body: JSON.stringify(formData),
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+        }
+    })
+    return await response.json()
+}
+
+/**
+ * Calls the ajax request and displays response/error message
+ *
+ * @param formData {object} values from form
+ */
+function sendRequest(formData) {
+    fetchData(formData).then(function (data) {
+        if (data.status) {
+            document.querySelector('.statusMessage').textContent = 'Success!'
+        } else {
+            document.querySelector('.statusMessage').textContent = 'Server error.'
+        }
+    }).catch(function (err) {
+        document.querySelector('.statusMessage').textContent = 'Ajax error: ' + err
+    })
+}
