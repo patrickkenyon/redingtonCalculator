6b90249ac31010a6575b2a6b93961ceadbced2e8
Patrick <patrick.kenyon@gmail.com>
2018-12-02 19:03:25 +0000

    removed risky test

--- tests/Models/CalculationModelTest.php
+++ tests/Models/CalculationModelTest.php
@@ -17,18 +17,6 @@ class CalculationModelTest Extends TestCase
         "date" => "2018/12/02"
     ];
 
-    public function testLogCalculation_success()
-    {
-        $mockCalculationEntity = $this->createMock(CalculationEntity::class);
-
-        $mockCalculationEntity->method('getProbabilityOne')->willReturn($this->probabilityCalculatorData['probabilityOne']);
-        $mockCalculationEntity->method('getProbabilityTwo')->willReturn($this->probabilityCalculatorData['probabilityTwo']);
-        $mockCalculationEntity->method('getResult')->willReturn($this->probabilityCalculatorData['result']);
-        $mockCalculationEntity->method('getCalcType')->willReturn($this->probabilityCalculatorData['calculation']);
-        $mockCalculationEntity->method('getDate')->willReturn($this->probabilityCalculatorData['date']);
-
-    }
-
     public function testLogCalculation_success_fileIsWritable()
     {
         $this->assertFileIsWritable(CalculationModel::CALCULATION_LOG);
